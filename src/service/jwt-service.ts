1b74b8a5447624adc87c672d9d5a9fd1ec613993
Alexsandr3 <fam_86@mail.ru>
2022-11-26 15:34:30 +0100

    add fix access token

--- src/service/jwt-service.ts
+++ src/service/jwt-service.ts
@@ -6,7 +6,7 @@ import {PayloadDto} from "../dtos/payload-dto";
 
 class JwtService {
     async generateTokens(userDto: UserDto | PayloadDto): Promise<TokensType> {
-        const accessToken = jwt.sign({userId: userDto.userId}, settings.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
+        const accessToken = jwt.sign({userId: userDto.userId}, settings.ACCESS_TOKEN_SECRET, {expiresIn: '1m'})
         const refreshToken = jwt.sign({userId: userDto.userId, deviceId: userDto.deviceId}, settings.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
         return new TokensType(accessToken, refreshToken)
     }
